3055e5406a2a4647d350a63d7382e1eda8a4f623
seapage <kontakt@seapage.pl>
2021-03-27 10:57:48 +0100

    Write some unit test

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,3 +1,20 @@
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+import { addKeyToLetterArray, replaceLetterOnIndex } from './utils';
+
+describe('addKeyToLetterArray', () => {
+    test('empty object', () => {
+        expect(addKeyToLetterArray({})).toEqual({});
+    });
+    test('correct added key to array', () => {
+        expect(addKeyToLetterArray({ c: ['a', 'b'] })).toEqual({ c: ['c', 'a', 'b'] });
+    });
+});
+
+describe('replaceLetterOnIndex', () => {
+    test('Replace one letter for second', () => {
+        expect(replaceLetterOnIndex('hello', 1, 'i')).toEqual('hillo');
+    });
+    test('Replace letter on index not existing on string', () => {
+        console.log(replaceLetterOnIndex('', 1, 'i'));
+        expect(replaceLetterOnIndex('', 1, 'i')).toEqual('');
+    });
 });
